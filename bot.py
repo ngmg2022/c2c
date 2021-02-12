e4f0b2f34253fe9589ae1e1662839bbcfcfaaf2a
SLdevilX <52373359+SLdevilX@users.noreply.github.com>
2021-02-12 13:48:53 +0530

    Btn

--- bot.py
+++ bot.py
@@ -24,13 +24,14 @@ except:
 @datgbot.on(events.NewMessage(pattern="/start"))
 async def _(event):
     ok = await datgbot(GetFullUserRequest(event.sender_id))
-    await event.reply(f"𝙷𝚎 𝚑𝚎`{ok.user.first_name}`!\n\n𝙸 𝚊𝚖 𝚊 𝙲𝚑𝚊𝚗𝚗𝚕𝚎 𝙿𝚘𝚜𝚝𝚎𝚛 𝚋𝚘𝚝. 𝙿𝚛𝚎𝚜𝚜 /help 𝚝𝚘 𝚐𝚎𝚝 𝚒𝚗𝚏𝚘 \n\nI can be used in only two channels (one user) at a time..\n\n[🤘](https://telegra.ph/file/1eca514b5e6202b1d92b3.jpg)", 
-    buttons=[Button.url("🤝Main Group🛰️", url="t.me/danuma01"), Button.url("📝Bot News🛸", url="https://t.me/dbotai"), Button.url("✈️Developer✈️", url="https://lasiya.ml")], link_preview=True)
+    await event.reply(f"𝙷𝚎 𝚑𝚎`{ok.user.first_name}`!\n\n𝙸 𝚊𝚖 𝚊 𝙲𝚑𝚊𝚗𝚗𝚕𝚎 𝙿𝚘𝚜𝚝𝚎𝚛 𝚋𝚘𝚝. \n 𝙿𝚛𝚎𝚜𝚜 /help 𝚝𝚘 𝚐𝚎𝚝 𝚒𝚗𝚏𝚘 \nI can be used in only two channels (one user) at a time..\n[🤘](https://telegra.ph/file/1eca514b5e6202b1d92b3.jpg)", 
+    buttons=[Button.url("🤝Main Group🛰️", url="t.me/danuma01"), Button.url("📝Bot News🛸", url="https://t.me/Dbotai")],
+    buttons=[Button.url("✈️Developer✈️", url="https://lasiya.ml")Button.url("🤙 Contact Dev 🛶", url="https://t.me/Danuma_admin_bot")], link_preview=True)
 
 
 @datgbot.on(events.NewMessage(pattern="/help"))
 async def helpp(event):
-    await event.reply("**Help**\n\nThis bot will send all new posts in one channel to the other channel (without forwarded tag).\nIt can be used only in two channels at a time, \n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!\n\nLiked the bot? Drop a ♥ to Danuma project")
+    await event.reply("**Help**\n\nThis bot will send all new posts in one channel to the other channel (without forwarded tag).\nIt can be used only in two channels at a time, \n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!\n\nLiked the bot? Drop a ♥Danuma projectject")
 
 @datgbot.on(events.NewMessage(incoming=True, chats=frm)) 
 async def _(event): 
